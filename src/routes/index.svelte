b1a7ab546b1d8979f3fa1b18ebfd8bdb967e2193
JPBlancoDB <jpblancodb@gmail.com>
2019-11-24 17:36:56 +0100

    add base template for article

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,23 +1,71 @@
-<script>
-  import { onMount } from "svelte";
-  import Article from "../components/Article";
-  import Loading from "../components/Loading.svelte";
+<style>
+  a {
+    text-decoration: none;
+  }
+  .card {
+    padding: 20px;
+    margin-bottom: 10px;
+    border: 2px solid #bababa;
+    box-shadow: 3px 3px 0px #bababa;
+  }
+  .tags {
+    margin: 10px 0;
+  }
+  .tag {
+    border-radius: 100px;
+    background-color: #eaeaea;
+    padding: 2px 8px;
+    margin-right: 10px;
+  }
 
-  let articles = [];
+  @media (max-width: 640px) {
+    .tag {
+      margin-right: 5px;
+      font-size: 0.6rem;
+    }
+  }
+  .article-engagement {
+    margin-right: 20px;
+  }
+  .article-engagement img {
+    height: 20px;
+    min-width: 26px;
+    vertical-align: -5px;
+  }
+  .flex-container {
+    display: flex;
+  }
+</style>
 
-  onMount(async () => {
-    const res = await fetch(`https://dev.to/api/articles?username=jpblancodb`);
-    articles = await res.json();
-  });
-</script>
+<a href="/">
+  <div class="card">
+    Nov 24
+    <h1>Create a blog with Svelte and DEV.to API</h1>
 
-{#each articles as article}
-  <Article
-    title={article.title}
-    description={article.description}
-    date={article.readable_publish_date}
-    tags={article.tag_list}
-    url={article.canonical_url}
-    comments={article.comments_count}
-    likes={article.positive_reactions_count} />
-{/each}
+    <div class="tags">
+      <span class="tag">#svelte</span>
+      <span class="tag">#javascript</span>
+      <span class="tag">#tutorial</span>
+      <span class="tag">#blog</span>
+    </div>
+
+    <p>How to create a blog using dev.to API and svelte</p>
+
+    <div class="flex-container">
+      <div class="article-engagement">
+        <img
+          alt="Reactions"
+          src="https://practicaldev-herokuapp-com.freetls.fastly.net/assets/reactions-stack-ee166e138ca182a567f74c986b6f810f670f4d199aca9c550cc7e6f49f34bd33.png" />
+        <span>10</span>
+      </div>
+
+      <div class="article-engagement">
+        <img
+          alt="Comments"
+          src="https://practicaldev-herokuapp-com.freetls.fastly.net/assets/comments-bubble-9958d41b969a1620c614347d5ad3f270ab49582c1d9f82b617a6b4156d05dda0.png" />
+        <span>3</span>
+      </div>
+
+    </div>
+  </div>
+</a>
