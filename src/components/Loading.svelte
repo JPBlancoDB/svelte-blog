25c538d2875778fe505ea4f96266bc0061a80b50
JPBlancoDB <jpblancodb@gmail.com>
2019-11-24 23:05:19 +0100

    Add onMount for getting data after render and loading component

---
+++ src/components/Loading.svelte
@@ -0,0 +1,4 @@
+<figure>
+  <img alt="Borat" src="great-success.png" />
+  <figcaption>Loading...</figcaption>
+</figure>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,13 @@
-<script context="module">
-  export async function preload() {
+<script>
+  import Loading from "../components/Loading.svelte";
+  let articles = [];
+
+  onMount(async () => {
     const res = await this.fetch(
       `https://dev.to/api/articles?username=jpblancodb`
     );
-    return { articles: await res.json() };
-  }
-</script>
-
-<script>
-  export let articles;
+    articles = res.json();
+  });
 </script>
 
 <style>
@@ -46,36 +45,40 @@
   }
 </style>
 
-{#each articles as article}
-  <a href={article.canonical_url}>
-    <div class="card">
-      {article.readable_publish_date}
-      <h1>{article.title}</h1>
+{#if articles.length > 0}
+  {#each articles as article}
+    <a href={article.canonical_url}>
+      <div class="card">
+        {article.readable_publish_date}
+        <h1>{article.title}</h1>
 
-      <div class="tags">
-        {#each article.tag_list as tag}
-          <span class="tag">#{tag}</span>
-        {/each}
-      </div>
-
-      <p>{article.description}</p>
-
-      <div class="flex-container">
-        <div class="article-engagement">
-          <img
-            alt="Reactions"
-            src="https://practicaldev-herokuapp-com.freetls.fastly.net/assets/reactions-stack-ee166e138ca182a567f74c986b6f810f670f4d199aca9c550cc7e6f49f34bd33.png" />
-          <span>{article.positive_reactions_count}</span>
+        <div class="tags">
+          {#each article.tag_list as tag}
+            <span class="tag">#{tag}</span>
+          {/each}
         </div>
 
-        <div class="article-engagement">
-          <img
-            alt="Comments"
-            src="https://practicaldev-herokuapp-com.freetls.fastly.net/assets/comments-bubble-9958d41b969a1620c614347d5ad3f270ab49582c1d9f82b617a6b4156d05dda0.png" />
-          <span>{article.comments_count}</span>
-        </div>
+        <p>{article.description}</p>
 
+        <div class="flex-container">
+          <div class="article-engagement">
+            <img
+              alt="Reactions"
+              src="https://practicaldev-herokuapp-com.freetls.fastly.net/assets/reactions-stack-ee166e138ca182a567f74c986b6f810f670f4d199aca9c550cc7e6f49f34bd33.png" />
+            <span>{article.positive_reactions_count}</span>
+          </div>
+
+          <div class="article-engagement">
+            <img
+              alt="Comments"
+              src="https://practicaldev-herokuapp-com.freetls.fastly.net/assets/comments-bubble-9958d41b969a1620c614347d5ad3f270ab49582c1d9f82b617a6b4156d05dda0.png" />
+            <span>{article.comments_count}</span>
+          </div>
+
+        </div>
       </div>
-    </div>
-  </a>
-{/each}
+    </a>
+  {/each}
+{:else}
+  <Loading />
+{/if}
